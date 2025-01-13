d5ea5c3337b78f2da0893db915bee170ce9bde40
Vitalii Khoruzhyi <avalor1991@gmail.com>
2025-01-13 09:18:42 -0500

    Update config.py

--- config/config.py
+++ config/config.py
@@ -9,10 +9,10 @@ config = {
     'interval': '15m',
     'period': '90d',
     'sleep_time': 1,
-    'api_key': '6751309736a5cd0001377cfd',
-    'api_secret': '763b3ebd-81c7-4c27-9bfc-f702761375ae',
-    'passphrase': 'avalor335620cash',
+    'api_key': ,
+    'api_secret': ,
+    'passphrase': ,
     'check_interval': 1,
-    'telegram_token': '7705839955:AAF8GtJiEbUVbVUrICT8d7mTSuW3AET74yg',
+    'telegram_token': ,
     'telegram_chat_id': '1086682233'
 }
